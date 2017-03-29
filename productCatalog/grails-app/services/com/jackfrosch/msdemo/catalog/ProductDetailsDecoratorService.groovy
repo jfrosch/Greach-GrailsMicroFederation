e11461814095b32ab220e2cb2ba257700847fc73
jackfrosch <jfrosch@objistics.com>
2017-03-30 00:22:21 +0200

    Workshop lab 6 solution

--- productCatalog/grails-app/services/com/jackfrosch/msdemo/catalog/ProductDetailsDecoratorService.groovy
+++ productCatalog/grails-app/services/com/jackfrosch/msdemo/catalog/ProductDetailsDecoratorService.groovy
@@ -1,11 +1,13 @@
 package com.jackfrosch.msdemo.catalog
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import grails.plugins.rest.client.RestBuilder
 import grails.transaction.Transactional
 
 @Transactional
 class ProductDetailsDecoratorService {
 
+    @HystrixCommand(fallbackMethod = "defaultRecommendations")
     List<Product> findRecommendations(Product product) {
         RestBuilder rest = new RestBuilder()
         def resp = rest.get("http://localhost:8765/recommendation-engine/findRecommendation/${product.id}")
@@ -18,4 +20,7 @@ class ProductDetailsDecoratorService {
         recommendedProductIds ? Product.where { id in recommendedProductIds}.list() : []
     }
 
+    List<Product> defaultRecommendations(Product product) {
+        Product.findAllByTagsIlike('%cat%')
+    }
 }
