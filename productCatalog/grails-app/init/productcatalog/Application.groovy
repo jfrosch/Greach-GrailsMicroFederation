c51b3fe232d4edd3db9078315de619160f389589
jackfrosch <jfrosch@objistics.com>
2017-03-29 21:24:27 +0200

    Workshop through Lab 3 completed

--- productCatalog/grails-app/init/productcatalog/Application.groovy
+++ productCatalog/grails-app/init/productcatalog/Application.groovy
@@ -2,11 +2,7 @@ package productcatalog
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.cloud.netflix.hystrix.EnableHystrix
 
-@EnableEurekaClient
-@EnableHystrix
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
--- productCatalog/grails-app/services/com/jackfrosch/msdemo/catalog/ProductDetailsDecoratorService.groovy
+++ productCatalog/grails-app/services/com/jackfrosch/msdemo/catalog/ProductDetailsDecoratorService.groovy
@@ -1,16 +1,14 @@
 package com.jackfrosch.msdemo.catalog
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import grails.plugins.rest.client.RestBuilder
 import grails.transaction.Transactional
 
 @Transactional
 class ProductDetailsDecoratorService {
 
-    @HystrixCommand(fallbackMethod = "defaultRecommendations")
     List<Product> findRecommendations(Product product) {
         RestBuilder rest = new RestBuilder()
-        def resp = rest.get("http://localhost:8765/recommendation-engine/findRecommendation/${product.id}")
+        def resp = rest.get("http://localhost:8092/findRecommendation/${product.id}")
 
         if(resp.status == 500) {
             throw new RuntimeException("Error calling recommendation engine")
@@ -20,7 +18,4 @@ class ProductDetailsDecoratorService {
         recommendedProductIds ? Product.where { id in recommendedProductIds}.list() : []
     }
 
-    List<Product> defaultRecommendations(Product product) {
-        Product.findAllByTagsIlike('%cat%')
-    }
 }
